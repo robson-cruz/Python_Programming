a5435f955575e4de22ed1bf4b85df4fdf10845ea
Robson Cruz <rcflorestal@yahoo.com.br>
2022-01-04 22:48:27 -0300

    subseting list of list

---
+++ Subsetting_lists_of_lists.py
@@ -0,0 +1,11 @@
+###--> Subsetting lists of lists #
+
+# Create the house list
+house = [['hallway', 11.25],
+         ['kitchen', 18.0],
+        ['living room', 20.0],
+        ['bedroom', 10.75],
+        ['bathroom', 9.5]]
+
+# print out the area of the bathroom
+print(house[-1][1])
