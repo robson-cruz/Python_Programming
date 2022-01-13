13fd6c9a542609601515c0770b91fbeb7cd298e0
Robson Cruz <rcflorestal@yahoo.com.br>
2022-01-12 21:43:34 -0300

    rename columns

--- 1_Creating_a_DataFrame.py
+++ 1_Creating_a_DataFrame.py
@@ -8,16 +8,22 @@ df = pd.DataFrame({
         'Shelf': ['A', 'B', 'C', 'D', 'E']
 })
 
-print(df)
-
-# Get the number of columns in the dataframe
-n_col = df.shape[1]
-print()
-print('Number of Columns:', n_col)
+print(df, end='\n\n')
 
 # Get the number of rows
 n_rows = df.shape[0]
 print('Number of Rows:', n_rows)
+
+# Get the number of columns in the dataframe
+n_col = df.shape[1]
+print('Number of Columns:', n_col, end='\n\n')
+
+# Get column names
+print('Column names:', df.columns, end='\n\n')
+
+# Replace 'fruit' column name with 'item'
+df.columns = ['Item', 'Aisle', 'Shelf']
+print(df)
 
 # save as .csv
 df.to_csv("df.csv", index=False)
