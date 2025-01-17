8db9b2cb5a90c7d41a23562ac42df86e495e83d9
Robson Cruz <22680143+robson-cruz@users.noreply.github.com>
2025-01-17 14:39:19 -0300

    Add multiple tests and Exception test

--- Unit_Test/pytest_framework/example_02/test_util.py
+++ Unit_Test/pytest_framework/example_02/test_util.py
@@ -3,6 +3,19 @@ import pytest
 from util import is_even
 
 
-@pytest.mark.parametrize("number, result", zip(range(6), [True, False, True, False, True, False]))
+@pytest.mark.parametrize("number, result", [
+    (0, True),
+    (1, False),
+    (2, True),
+    (3, False),
+    (4, True),
+    (5, False)
+])
 def test_many(number, result):
     assert is_even(number) is result
+
+
+def test_exception():
+    with pytest.raises(TypeError) as e:
+        is_even("abc")
+        assert e.value.args[0] == "Invalid input type, only integers allowed."
