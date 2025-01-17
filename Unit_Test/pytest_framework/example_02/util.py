993267b011a2ed8a16eee8a2d1cb7d57cd3c95f6
Robson Cruz <22680143+robson-cruz@users.noreply.github.com>
2025-01-17 14:40:50 -0300

    update function

--- Unit_Test/pytest_framework/example_02/util.py
+++ Unit_Test/pytest_framework/example_02/util.py
@@ -1,4 +1,26 @@
 def is_even(number):
+    """
+    Check if a number is even.
+
+    Parameters:
+    number (int): The number to check.
+
+    Returns:
+    bool: True if the number is even, False otherwise.
+
+    Raises:
+    TypeError: If the input is not an integer.
+
+    Example:
+    >>> is_even(4)
+    True
+    >>> is_even(3)
+    False
+    >>> is_even("abc")
+    Traceback (most recent call last):
+        ...
+    TypeError: Invalid input type, only integers allowed.
+    """
     if isinstance(number, int):
         return number % 2 == 0
     raise TypeError("Invalid input type, only integers allowed.")
